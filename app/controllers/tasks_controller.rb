acb862ad1a7d32b67e61bb214b5521d1e2f01f4e
Thibaut ANDRY <andrythibaut@gmail.com>
2018-11-21 11:21:27 +0100

    task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,30 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  def show
+    @task = Task.find(params[:id])
+  end
+  def new
+    # We'll see that in a moment.
+    @task = Task.new
+  end
+  def create
+    @task = Task.create(params[task])
+
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:title, :details])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
 end
